cee9adfcfbe3a528b811f2ba94754ef025dca950
mehulgondaliya <mehulgondaliya@knovator.in>
2024-04-17 13:18:26 +0530

    .

---
+++ app/widgets/loader/ReactLoadingSkeleton.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+
+const DynamicSkeleton = ({ shape, count, animated }) => {
+  const renderSkeletons = () => {
+    const skeletons = [];
+    for (let i = 0; i < count; i++) {
+      if (shape === 'circle') {
+        skeletons.push(
+          <div key={i} style={{ margin: '10px' }}>
+            <Skeleton circle={true} height={50} width={50} enableAnimation={animated} />
+          </div>
+        );
+      } else {
+        skeletons.push(
+          <div key={i} style={{ margin: '10px' }}>
+            <Skeleton height={50} width={100} />
+          </div>
+        );
+      }
+    }
+    return skeletons;
+  };
+
+  return <div style={{ display: 'flex', flexWrap: 'wrap' }}>{renderSkeletons()}</div>;
+};
+
+export default DynamicSkeleton;
---
+++ app/widgets/table/ResponsiveTable.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './ResponsiveTable.css'
+import DynamicSkeleton from '../loader/ReactLoadingSkeleton';
+
+
+const ResponsiveTable = ({ data = [1,1,1] }) => {
+  return (
+    <div className="responsive-table">
+      <div className="table-header">
+        <div className="column">Header 1</div>
+        <div className="column">Header 2</div>
+        <div className="column">Header 3</div>
+      </div>
+      <div className="table-body">
+        {data.map((item, index) => (
+          <div className="row" key={index}>
+            <div className="column">{item.column1 || <DynamicSkeleton shape="circle" count={5} animated={true} />}</div>
+            <div className="column">{item.column2 ||  <DynamicSkeleton shape="box" count={3} animated={false} />}</div>
+            <div className="column">{item.column3 }</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ResponsiveTable;
